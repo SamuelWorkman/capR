8dc52bf04c4b90ec0ca87b14c88770450233d66a
borghenry <borghenry@gmail.com>
2018-06-27 18:32:09 +0100

    first commit of function that download data and merge common variables (id, year, topic and subtopic) into 1 df

---
+++ CapRpackage/R/cap_df.R
@@ -0,0 +1,92 @@
+## App that downloads complete datasets as a dataframe
+
+library(shiny)
+library(shinyWidgets)
+library(miniUI)
+library(leaflet)
+library(ggplot2)
+library(tidyverse)
+
+cap_df <- function() {
+
+  data <- jsonlite::fromJSON("https://www.comparativeagendas.net/api/datasets/metadata") %>%
+    rename(Country = country, Type = name, Units = stats_observations,
+           from = stats_year_from, to = stats_year_to) %>%
+    mutate(url = paste0("https://comparativeagendas.s3.amazonaws.com/datasetfiles/", datasetfilename),
+           country_type = paste(Country, Type, sep = "_"))
+
+  allchoices_country <- c("Select all", unique(data$Country))
+  allchoices_category <- c("Select all", unique(data$category))
+
+  ui <- miniPage(
+    gadgetTitleBar("Select CAP data",
+                   right = miniTitleBarButton("done", "Download", primary = TRUE)),
+    miniTabstripPanel(
+      miniTabPanel("Parameters", icon = icon("sliders"),
+                   miniContentPanel(
+                     fillRow(pickerInput(
+                       inputId = "category",
+                       label = "Select type of activity",
+                       choices = unique(data$category),
+                       multiple = TRUE,
+                       options = list(`actions-box` = TRUE)
+                     ),
+                     pickerInput(
+                       inputId = "country",
+                       label = "Select country",
+                       choices = "", selected = "",
+                       multiple = TRUE,
+                       options = list(`actions-box` = TRUE)
+                     ))
+                   )),
+      miniTabPanel("Data", icon = icon("table"),
+                   miniContentPanel(DT::DTOutput("tabledt"))
+      )
+    )
+  )
+
+  server <- function(input, output, session) {
+
+    observeEvent(
+      input$category,
+      updateSelectInput(session, "country", "Select country",
+                        choices = unique(data$Country[data$category==input$category])))
+
+    select_country <- reactive({
+      dfselect <- data %>%
+        filter(category %in% input$category & Country %in% input$country) %>%
+        select(country_type, url) %>%
+        arrange(country_type)
+      # 
+      # dfselect <- filter(data, Country=="Italy") %>%
+      # select(country_type, url)
+
+      df <- purrr::map(.x = dfselect$url, .f = ~read_csv(.x)) %>%
+        set_names(dfselect$country_type) %>%  
+        purrr::map(.x = ., possibly(.f = ~dplyr::select(., id, year, subtopic, majortopic), otherwise = NA)) %>%
+        purrr::map(.x = ., .f = ~dplyr::mutate(., id = ifelse(is.numeric(id), as.character(id), id))) %>%
+        purrr::map_df(., I, .id ="dataframe")
+      df
+    })
+
+
+    output$tabledt <- DT::renderDT(DT::datatable(select_country(),
+                                      options = list(searching = FALSE,
+                                               pageLength = 5,
+                                               columnDefs = list(list(width = '20%',
+                                              targets = list(1, 2)))
+                                                      ),
+                                        rownames = FALSE))
+
+    observeEvent(input$done, {
+
+      stopApp(
+        select_country()
+      )
+    })
+  }
+
+  runGadget(ui, server)
+}
+
+# try <- cap_df()
